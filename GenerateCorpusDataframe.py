71505d6a157d5f4f1de7f3fcb5742a1f6702bd82
naitikbhise <naitikbhise@gmail.com>
2019-11-12 13:37:51 -0500

    Added the sentence tokenize function

--- GenerateCorpusDataframe.py
+++ GenerateCorpusDataframe.py
@@ -23,7 +23,61 @@ def getDataframe(year):
     data["second"] = data['Created At'].map(lambda x: dater(x,4))
     return data[data["year"]==str(year)][['Title','Post Type','Number of Comments','Points','Author']].copy()
 
+def is_noun(tag):
+    return tag in ['NN', 'NNS', 'NNP', 'NNPS']
+
+def is_verb(tag):
+    return tag in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+def is_adverb(tag):
+    return tag in ['RB', 'RBR', 'RBS']
+
+def is_adjective(tag):
+    return tag in ['JJ', 'JJR', 'JJS']
+
+def penn_to_wn(tag):
+    if is_adjective(tag):
+        return wn.ADJ
+    elif is_noun(tag):
+        return wn.NOUN
+    elif is_adverb(tag):
+        return wn.ADV
+    elif is_verb(tag):
+        return wn.VERB
+    return None
+
+def sentence_split(sentence):
+    text = nltk.word_tokenize(sentence)
+    pos_text = nltk.pos_tag(text)
+    new_text = []
+    grab = False
+    for word,pos in pos_text:
+        #print(word)
+        #print(pos)
+        tag = penn_to_wn(pos)
+        if pos == 'NNP' and grab == True:
+            grab = False
+            #print(word)
+            lemmatizedWord += " " + wordnet_lemmatizer.lemmatize(word,tag)
+            new_text.append(lemmatizedWord)
+            lemmatizedWord = None
+        elif pos == 'NNP' and grab == False:
+            lemmatizedWord = wordnet_lemmatizer.lemmatize(word,tag)
+            grab = True
+            #print(grab)
+        elif pos != 'NNP':
+            if lemmatizedWord:
+                new_text.append(lemmatizedWord)
+            if tag is None:
+                lemmatizedWord = word
+            else:
+                lemmatizedWord = wordnet_lemmatizer.lemmatize(word,tag)
+            new_text.append(lemmatizedWord)
+            lemmatizedWord = None
+            grab = False
+    return new_text
+
 def addTokenizedColumnofTitle(data):
-    data['tokenized_title'] = data['Title'].map(lambda x:nltk.word_tokenize(x.lower()))
+    data['tokenized_title'] = data['Title'].map(lambda x:sentence_split(x))
     return data
 
